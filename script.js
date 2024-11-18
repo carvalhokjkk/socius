52b3df8d30d0a1c1ec90a16771ea8ff68150736b
Elias Carvalho <carvalho2006elias@gmail.com>
2024-11-18 18:14:24 -0300

    ajustes finais compatibilidade mobile

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function scrol(element_id) {
+function scroll_to(element_id) {
 	document.getElementById(element_id).scrollIntoView({
 		behavior: 'smooth',
 		block: 'start'
 	});
 };
+
+
+function showNotification() {
+	const notification_area = document.getElementById('notification-menu')
+	const body = document.body
+	notification_area.style.right = '0px'
+	body.style.touchAction = 'none'
+}
+
+function hideNotification() {
+	const body = document.body
+	const notification_area = document.getElementById('notification-menu')
+	notification_area.style.right = '-400px'
+	body.style.touchAction = 'initial'
+}
